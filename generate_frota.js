7269e893e32c99ecf07fd011dab564e1f3c04161
Thiago Vinicius de Souza M Lacerda <thiago.lacerda@construtora.int>
2026-07-30 20:02:34 -0300

    atualizando link ngrok apos restart

--- generate_frota.js
+++ generate_frota.js
@@ -4,7 +4,7 @@ const path = require('path');
 const backendPath = path.join(__dirname, 'backend');
 const outputPath = path.join(__dirname, 'src', 'data', 'frota.json');
 
-const BASE_URL = "https://7faa-177-86-10-25.ngrok-free.app"; // Link gerado pelo Ngrok
+const BASE_URL = "https://419a-177-86-10-25.ngrok-free.app"; // Link gerado pelo Ngrok
 
 function getFiles(dir) {
   let results = [];
